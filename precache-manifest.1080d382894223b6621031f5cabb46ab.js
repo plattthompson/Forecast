130ada088f763a904f246f69ffff6fc4e7a16dc8
Platt Thompson <plattthompsonprogramming@gmail.com>
2019-02-18 20:51:03 -0800

    Updates

---
+++ precache-manifest.1080d382894223b6621031f5cabb46ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a396a40dca90471a7e3e",
+    "url": "/forecast/static/js/runtime~main.a396a40d.js"
+  },
+  {
+    "revision": "6841f24de013732a6bd2",
+    "url": "/forecast/static/js/main.6841f24d.chunk.js"
+  },
+  {
+    "revision": "ad01f2070095b9a3c16d",
+    "url": "/forecast/static/js/2.ad01f207.chunk.js"
+  },
+  {
+    "revision": "6841f24de013732a6bd2",
+    "url": "/forecast/static/css/main.2fb7c65c.chunk.css"
+  },
+  {
+    "revision": "ad01f2070095b9a3c16d",
+    "url": "/forecast/static/css/2.28da17b9.chunk.css"
+  },
+  {
+    "revision": "cde067ca610154a6b3b6a387a84047b6",
+    "url": "/forecast/index.html"
+  }
+];
