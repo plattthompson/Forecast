d845d9539ecfd1c319db8a6bfde5e2062dc38680
Platt Thompson <plattthompsonprogramming@gmail.com>
2019-03-31 01:40:38 -0700

    Updates

--- precache-manifest.4bac235b7e294183933da9360b45c61c.js
+++ precache-manifest.4bac235b7e294183933da9360b45c61c.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/forecast/static/js/runtime~main.a396a40d.js"
   },
   {
-    "revision": "a6691977671d5e0499b2",
-    "url": "/forecast/static/js/main.a6691977.chunk.js"
+    "revision": "896d446dba56fd306bf6",
+    "url": "/forecast/static/js/main.896d446d.chunk.js"
   },
   {
     "revision": "ad01f2070095b9a3c16d",
     "url": "/forecast/static/js/2.ad01f207.chunk.js"
   },
   {
-    "revision": "a6691977671d5e0499b2",
-    "url": "/forecast/static/css/main.2fb7c65c.chunk.css"
+    "revision": "896d446dba56fd306bf6",
+    "url": "/forecast/static/css/main.44b2e571.chunk.css"
   },
   {
     "revision": "ad01f2070095b9a3c16d",
     "url": "/forecast/static/css/2.28da17b9.chunk.css"
   },
   {
-    "revision": "859e21c70dd74a7305c27ebee9b57152",
+    "revision": "ad5011c5cfba272e349a1d5da66f12d5",
     "url": "/forecast/index.html"
   }
 ];
